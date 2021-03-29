82ea10ec5c9da65d0ae5b5163d2d625f2fc832ac
Miki Harian <mikiharian94@gmail.com>
2021-03-30 06:58:25 +0700

    chore(config): build configuration

---
+++ src/utils/swregister.js
@@ -0,0 +1,25 @@
+/**
+ * Registers our Service Worker on the site
+ * Need more? check out:
+ * https://github.com/GoogleChrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
+ */
+
+const Config = require('./config');
+
+if (!Config.DEV && typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      // If Successfully Register Service Worker
+      console.log(
+        '%c Service worker was successfully installed. Now you can access even when you\'re offline',
+        'background: #ddd; color: green; padding: 20px'
+      );
+    })
+    .catch((e) => {
+      if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') {
+        return;
+      }
+      throw ('Error during service worker registration:', e);
+    });
+}
